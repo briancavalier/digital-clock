a380d3750889572efbc0412bbb8272e50ba7ef21
Brian Cavalier <brian@briancavalier.com>
2010-07-29 21:06:51 -0400

    adding some touch and mobile webkit stuff--not really working yet, just some preliminary dabbling genericized theme switching code cleanup

--- digital-clock/clock.js
+++ digital-clock/clock.js
@@ -1,11 +1,20 @@
 dojo.ready(function() {
-	var dimTime = 10 * 1000
+	var doc = document
+		,body = doc.body
+		,$ = dojo.query
+		,dimTime = 10 * 1000
 		,dimTimeout
 		,hasLocalStorage = ('localStorage' in window) && window['localStorage'] !== null
 		,prefsKey = "digitalClock.prefs"
 		,prefs = loadPrefs({ theme: "green", hours: 12 });
 		;
-
+		
+	function isTouch() {
+		var el = doc.createElement('div');
+		el.setAttribute('ongesturestart', 'return;');
+		return typeof el.ongesturestart == "function";
+	}
+	
 	function loadPrefs(defaults) {
 		var p;
 		if(hasLocalStorage) p = window.localStorage.getItem(prefsKey);
@@ -18,20 +27,20 @@ dojo.ready(function() {
 	}
 
 	function updateTime() {
-		dojo.query('.colon .element').toggleClass('on');
-		dojo.query('.number').removeClass(["d0", "d1", "d2", "d3", "d4", "d5", "d6", "d7", "d8", "d9"]);
+		$('.colon .element').toggleClass('on');
+		$('.number').removeClass(["d0", "d1", "d2", "d3", "d4", "d5", "d6", "d7", "d8", "d9"]);
 		var now = new Date()
 			,h = now.getHours()
 			,m = now.getMinutes()
 			,s = now.getSeconds()
-			,n = dojo.query('.number')
+			,n = $('.number')
 			,hours = prefs.hours
 			,ap = (h >= hours) ? "pm" : "am"
 			;
 		// If 12hr clock, adjust h for display, and set AM/PM
 		if(hours == 12) {				
 			h = (h == 0) ? 12 : (h > 12) ? h % 12 : h;
-			dojo.query('.ampm').removeClass(["am", "pm"]).addClass(ap);
+			$('.ampm').removeClass(["am", "pm"]).addClass(ap);
 		}
 
 		// Javascript Pittsburgh-ese .. n.at :)
@@ -45,12 +54,13 @@ dojo.ready(function() {
 	}
 			
 	function brighten() {
-		dojo.query(document.body).removeClass("dim");
+		$(body).removeClass("dim");
 		setupDim();
+		return true;
 	}
 	
 	function dim() {
-		dojo.query(document.body).addClass("dim");			
+		$(body).addClass("dim");			
 	}
 	
 	function setupDim() {
@@ -59,15 +69,15 @@ dojo.ready(function() {
 	}
 	
 	function setTheme(theme) {
-		dojo.query(document.body).removeClass(["green", "blue", "red", "lcd1", "blue-backlight"]).addClass(theme);
-		dojo.query('.controls .dot.on').removeClass('on');
-		dojo.query('.controls .dot.' + theme).addClass('on');
+		$(body).removeClass($(".theme").map(function(t) { return t.name; })).addClass(theme);
+		$('.controls .dot.on').removeClass('on');
+		$('.controls .dot.' + theme).addClass('on');
 		prefs.theme = theme;
 		savePrefs();
 	}
 	
 	function setHours(hours) {
-		dojo.query(document.body).removeClass(["hr12", "hr24"]).addClass('hr' + hours);
+		$(body).removeClass(["hr12", "hr24"]).addClass('hr' + hours);
 		prefs.hours = hours;
 		savePrefs();
 		updateTime();
@@ -79,8 +89,13 @@ dojo.ready(function() {
 	updateTime();
 	setupDim();
 	setInterval(updateTime, 1000);
-	dojo.query(document.body).onmousemove(brighten).onclick(brighten);
+
+	var events = isTouch() ? ["ontouchstart"] : ["onmousemove", "onclick"];	
+	$(".clock").forEach(function(node) {
+		for(var i=0; i<events.length; i++)
+			dojo.connect(node, events[i], brighten);
+	});
 	
-	dojo.query('.controls .dot').onclick(function(e) { setTheme(dojo.attr(this, "name")); });
-	dojo.query('.controls .hours').onclick(function(e) { setHours(1*dojo.attr(this, "name")); });	
+	$('.controls .dot').onclick(function(e) { setTheme(dojo.attr(this, "name")); });
+	$('.controls .hours').onclick(function(e) { setHours(1*dojo.attr(this, "name")); });	
 });
